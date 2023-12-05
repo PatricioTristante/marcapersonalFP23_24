c394b271d7a139b81d6ae606c5f6e8b776f6035a
Patricio Tristante <6808469@alu.murciaeduca.es>
2023-12-05 09:47:00 +0100

    Cambios a edit y create de estudiantes

--- app/Http/Controllers/EstudianteController.php
+++ app/Http/Controllers/EstudianteController.php
@@ -23,12 +23,26 @@ class EstudianteController extends Controller
             ->with("estudiante", Estudiante::findOrFail($id));
     }
 
-    public function putEdit($id) {
-        return view('estudiantes.edit')
-            ->with("estudiante", Estudiante::findOrFail($id));
+    public function putEdit(Request $request, $id) {
+        $estudiante = Estudiante::findOrFail($id);
+        $estudiante->nombre = $request->nombre;
+        $estudiante->apellidos = $request->apellidos;
+        $estudiante->direccion = $request->direccion;
+        $estudiante->votos = $request->votos;
+        $estudiante->ciclo = $request->ciclo;
+        $estudiante->save();
+
+        return redirect(action([self::class, 'getShow'], ['id' => $estudiante->id]));
     }
 
     public function getCreate(){
         return view('estudiantes.create');
+    }
+
+    public function store(Request $request){
+        //Por que patata
+        $estudiante = Estudiante::create($request->all());
+
+        return redirect(action([self::class, 'getShow'], ['id' => $estudiante->id]));
     }
 }
